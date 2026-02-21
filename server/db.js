e19deb07e10ddeb3e32aba47c8a8ec087ab81768
Aaron Cuen <aaroncuen020714@gmail.com>
2026-02-20 17:27:11 -0700

    db

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3360,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-console.log("HOST:", process.env.DB_HOST);
-console.log("PORT:", process.env.DB_PORT);
+
 db.connect(err => {
   if (err) {
     console.log("Error DB:", err);
